a5270ec906cae8ae1dc32c948d944e9b311ed2e8
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-02-21 12:43:22 +0100

    essaie avec transformation de la saisir user

--- __init__.py
+++ __init__.py
@@ -2,9 +2,18 @@ from cryptography.fernet import Fernet
 from flask import Flask, render_template_string, render_template, jsonify, request, json
 from urllib.request import urlopen
 import sqlite3
+import hashlib
+import base64
                                                                                                                                    
-
 app = Flask(__name__)
+# je transforme la saisir de l'utilisateur en clé fermet valide ( je la hash puis je la convertir en base64)
+
+def transformercle(custom_key: str) -> bytes:
+
+    hash_obj = hashlib.sha256(custom_key.encode())
+    digest = hash_obj.digest()
+    fernet_key = base64.urlsafe_b64encode(digest)
+    return fernet_key
 
 @app.route('/')
 def hello_world():
@@ -15,7 +24,8 @@ def encryptage():
     data = request.get_json()
     cle = data.get('cle')
     valeur = data.get('valeur')
-    f = Fernet(cle)
+    fernet_key = transformercle(cle)
+    f = Fernet(fernet_key)
     token = f.encrypt(valeur.encode())
     return jsonify({"result": token.decode()})
 
@@ -24,7 +34,8 @@ def decryptage():
     data = request.get_json()
     cle = data.get('cle')
     token = data.get('token')
-    f = Fernet(cle)
+    fernet_key = transformercle(cle)
+    f = Fernet(fernet_key)
     valeur = f.decrypt(token.encode()).decode()
     return jsonify({"result": valeur})
 
