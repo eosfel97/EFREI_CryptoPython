0c5ca52748031a7665c522031c011438a5c9f1f0
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-02-21 11:19:26 +0100

    exo 5 decrypt

--- __init__.py
+++ __init__.py
@@ -19,6 +19,14 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+        token_bytes = token.encode()            
+        valeur_bytes = f.decrypt(token_bytes)    
+        valeur = valeur_bytes.decode()           
+        return f"Valeur décryptée : {valeur}"
+
+
 if __name__ == "__main__":
   app.run(debug=True)
